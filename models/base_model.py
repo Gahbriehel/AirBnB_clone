60a92f24d78d3945aa57ba8c06c907a5b4595c48
Gahbriehel <babatise002@gmail.com>
2024-05-14 03:32:38 -0700

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -22,4 +22,15 @@ class BaseModel:
 
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
+    def save(self):
+        """
+        Updates the public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
 
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        obj_dict = self.__dict__
+        return obj_dict
