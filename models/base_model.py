08032a2ba37e0d298bd0802bf25c228aed8b6833
Donald <donumeh54@gmail.com>
2024-05-14 11:17:32 +0100

    updated basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,18 @@ class BaseModel:
     This defines all common attributes or methods for other classes
     """
     def __init__(self):
+        """
+        Base Model Instructor
+        """
         self.id = uuid4()
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        String format for BaseModel
+        """
+
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+
