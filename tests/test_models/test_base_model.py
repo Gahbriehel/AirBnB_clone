81265d637cf0f3b8c4232ce8cbeb7d85b8ab1e3a
Gahbriehel <babatise002@gmail.com>
2024-05-14 20:58:30 -0700

    Added tests for BaseModel instances

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,4 +7,16 @@ from models.base_model import BaseModel
 
 
 class Test_BaseModel(unittest.TestCase):
-	"""Tests for the BaseModel class"""
+    """Tests for the BaseModel class"""
+
+    def setUp(self):
+        """Instances to test BaseModel class"""
+        self.obj_1 = BaseModel()
+        self.obj_2 = BaseModel()
+
+    def test_class_attributes(self):
+        """Tests for the BaseModel class attributes"""
+        self.assertTrue(hasattr(self.obj_1, "id"))
+        self.assertTrue(hasattr(self.obj_1, "created_at"))
+        self.assertTrue(hasattr(self.obj_1, "updated_at"))
+        self.assertFalse(hasattr(self.obj_1, "kwargs"))
