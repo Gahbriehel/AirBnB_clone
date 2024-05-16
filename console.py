5220815f548f1c69eb7fa96efb8404b0e144d5f5
Gahbriehel <babatise002@gmail.com>
2024-05-16 14:41:00 -0700

    Updated command interpreter(console.py) to have 'create' and 'show' commands

--- console.py
+++ console.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import cmd
 import os
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """ Simple command processor example. """
     prompt = '(hbnb) '
     last_output = ''
+
+    __class_list = ["BaseModel"]
 
     def do_shell(self, line):
         """ Run a shell command """
@@ -28,6 +31,30 @@ class HBNBCommand(cmd.Cmd):
         """Empty Line"""
         pass
 
+    def do_create(self, line):
+        """ Creates a new instance of BaseModel
+        + saves it (to the JSON file) and prints the id
+        """
+        if not line:
+            print("** class name missing **")
+        elif line not in HBNBCommand.__class_list:
+            print("** class doesn't exist **")
+        else:
+            base_instance = BaseModel()
+            base_instance.save()
+            print(base_instance.id)
 
+    def do_show(self, line):
+        """ Prints the string representation of an instance based on the class name and id """
+        if not line:
+            print("** class name missing **")
+        elif line not in HBNBCommand.__class_list:
+            print("** class doesn't exist **")
+        else:
+            print("")
+
+
+
+    
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
