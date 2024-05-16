95dc7b8143ee30ba393d0b22fbdede0f7a6f5397
Gahbriehel <babatise002@gmail.com>
2024-05-16 09:42:21 -0700

    Created console.py

---
+++ console.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import cmd
+import os
+
+
+class HBNBCommand(cmd.Cmd):
+    """Simple command processor example."""
+    prompt = '(hbnb) '
+    last_output = ''
+
+    def do_shell(self, line):
+        """Run a shell command"""
+        print("running shell command:", line)
+        output = os.popen(line).read()
+        print(output)
+        self.last_output = output
+
+
+    def do_EOF(self, line):
+        """
+        End of File: Ctrl + d
+        """
+        return True
+
+    def do_quit(self, line):
+        """End of File
+        Use: Ctrl + d
+        """
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
