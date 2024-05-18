2fd904bd96cdfade7c452583f80c4a7419f5f159
Gahbriehel <babatise002@gmail.com>
2024-05-18 16:42:52 -0700

    fixed issues in tests for FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,24 +3,40 @@
 
 import unittest
 import json
+import os
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
 
 
 class TestFileStorage(unittest.TestCase):
     """ Tests for the FileStorage class """
+
     def setUp(self):
-        """Instances to test FileStorage class"""
+        """ Instances to test FileStorage class """
         FileStorage._FileStorage__objects = {}
         self.storage = FileStorage()
         self.obj_1 = BaseModel()
         self.obj_1.id = "1234"
         self.obj_dict = self.obj_1.to_dict()
-        self.file_path = FileStorage._FileStorage__file_path
+        self.file_path = "test_file.json"
+        FileStorage._FileStorage__file_path = self.file_path
+
+    def tearDown(self):
+        """ Cleaning up any created files """
+        FileStorage._FileStorage__objects = {}
+        try:
+            os.remove(self.file_path)
+        except FileNotFoundError:
+            pass
 
     def test_all(self):
         """ Tests for the all method """
-        self.assertEqual(self.storage.all(), {})
+        self.storage.new(self.obj_1)
+        all_objects = self.storage.all()
+        key = "BaseModel.1234"
+        self.assertIn(key, all_objects)
+        self.assertEqual(all_objects[key], self.obj_1)
 
     def test_new(self):
         """ Tests for the new method"""
