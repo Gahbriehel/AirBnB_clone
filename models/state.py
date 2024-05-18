fcda50a80af8d5bd9b97e40810be9c140f5f8f67
Donald <donumeh54@gmail.com>
2024-05-18 16:35:22 +0100

    modified state

--- models/state.py
+++ models/state.py
@@ -10,9 +10,3 @@ class State(BaseModel):
     State Model that inherits from the BaseModel
     """
     name = ""
-
-    def __init__(self, *arg, **kwargs):
-        """
-        Constructor for the State Model
-        """
-        super()__init__(**kwargs)
